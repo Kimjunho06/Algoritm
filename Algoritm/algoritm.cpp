8cbfffc29705a3ec744debf02e143f6699dbdfdf
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-03-13 19:07:21 +0900

    시간 변환

--- Algoritm/algoritm.cpp
+++ Algoritm/algoritm.cpp
@@ -12,12 +12,28 @@ int main() {
 
 	cout << max;*/
 
-	int n, cnt = 0;
+	/*int n, cnt = 0;
 	cin >> n;
 
 	for (int i = 2; i <= n; i++) {
 		if (n % i == 0) cnt++;
 	}
 
-	cout << cnt;
+	cout << cnt;*/
+
+	int hou = 0, min = 0, sec = 0, N;
+	cin >> N;
+
+	while (N >= 60) {
+		N -= 60;
+		min++;
+		if (min >= 60) {
+			min -= 60;
+			hou++;
+		}
+	}
+
+	sec = N;
+
+	cout << hou << " " << min << " " << sec;
 }
