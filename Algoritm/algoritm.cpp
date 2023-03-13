3cb4628f445486d8c0ff4257f811a9bfe785f8d3
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-03-13 18:52:57 +0900

    대금 만들기

--- Algoritm/algoritm.cpp
+++ Algoritm/algoritm.cpp
@@ -3,12 +3,21 @@
 using namespace std;
 
 int main() {
-	int a, b, max = 0;
+	/*int a, b, max = 0;
 	for (int i = 0; i < 3; i++) {
 		cin >> a >> b;
 		if (a * b > max)
 			max = a * b;
 	}
 
-	cout << max;
+	cout << max;*/
+
+	int n, cnt = 0;
+	cin >> n;
+
+	for (int i = 2; i <= n; i++) {
+		if (n % i == 0) cnt++;
+	}
+
+	cout << cnt;
 }
