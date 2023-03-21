bb3b9652fb5e97aeec811d4ffaa65ef74945f4fb
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-03-21 19:25:42 +0900

    GDC 그리디

--- Algoritm/algoritm.cpp
+++ Algoritm/algoritm.cpp
@@ -4,13 +4,18 @@ using namespace std;
 
 
 int main() {
-	int n, m;
-	cin >> n >> m;
+	int n;
+	cin >> n;
 
-	if (n == 1 || m == 1) cout << "Yes";
-	else if (n <= 2 && m <= 2) cout << "YES";
-	else cout << "NO";
+	if (n % 2 == 0) {
+		n /= 2;
+	}
+	else if (n % 2 == 1) {
+		n -= 1;
+		n /= 2;
+	}
 
+	cout << n;
 }
 
 
