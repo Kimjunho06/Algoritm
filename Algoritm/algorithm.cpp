9d7d3297639e4acb117971ddacf42ac3eefc6c74
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-04-04 20:12:56 +0900

    upper_bound, lower_bound 사용법 (#include <algorithm>)

--- Algoritm/algorithm.cpp
+++ Algoritm/algorithm.cpp
@@ -5,32 +5,14 @@ using namespace std;
 
 int main()
 {
-	int N, k, cnt = 0, result = 0;
-	cin >> N >> k;
+	int arr[10];
+	int x, y;
+	cin >> x >> y;
 
-	int low, mid, high;
-
-	low = 1; // first index + 1
-	high = k; // last index + 1
-
-	while (low <= high) {
-		mid = (low + high) / 2;
-		cnt = 0;
-
-		for (int i = 1; i <= N; i++) {
-			cnt += min(mid / i, N);
-		}
-
-		if (cnt < k) {
-			low = mid + 1;
-		}
-		else {
-			result = mid;
-			high = mid - 1;
-		}
-	}
-
-	cout << result;
+	// x 이상 y 이하의 값 찾기
+	upper_bound(arr, arr + 10, y) - lower_bound(arr, arr + 10, x);
+	// x의 개수
+	upper_bound(arr, arr + 10, y) - lower_bound(arr, arr + 10, y);
 }
 
 // k는 작고 큰지만 판별하는 것
