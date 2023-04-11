da62cdd13bb1f936d331ddd3aa8265dfd5f5de86
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-04-11 13:51:22 +0900

    스택 문제

--- Algoritm/algorithm.cpp
+++ Algoritm/algorithm.cpp
@@ -1,73 +1,58 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
-#include <vector>
+#include <stack>
 
 using namespace std;
-
-bool comp(string a, string b) {
-    if (a.length() < b.length()) {
-        return true;
-    }
-    else if (a.length() == b.length()) {
-        if (a < b) {
-            return true;
-        }
-    }
-    return false;
-}
-
-
-vector<int> solution(vector<string> words, vector<string> queries) {
-    vector<int> answer;
-    
-    vector<string> revwords = words;
-    for (int i = 0; i < revwords.size(); i++) {
-        reverse(revwords[i].begin(), revwords[i].end());
-    }
-
-    sort(words.begin(), words.end(), comp);
-    sort(revwords.begin(), revwords.end(), comp);
-
-    for (int i = 0; i < queries.size(); i++) {
-        int low, high, idx;
-        string strQuery = queries[i];
-
-        if (strQuery[0] == '?') {
-            reverse(strQuery.begin(), strQuery.end());
-            idx = strQuery.find('?');
-            for (int j = idx; j < strQuery.size(); j++) {
-                strQuery[j] = 'a';
-            }
-            low = lower_bound(revwords.begin(), revwords.end(), strQuery, comp) - revwords.begin();
-            
-            for (int j = idx; j < strQuery.size(); j++) {
-                strQuery[j] = 'z';
-            }
-            high = upper_bound(revwords.begin(), revwords.end(), strQuery, comp) - revwords.begin();
-
-        }
-        else {
-            idx = strQuery.find('?');
-            for (int j = idx; j < strQuery.size(); j++) {
-                strQuery[j] = 'a';
-            }
-            low = lower_bound(words.begin(), words.end(), strQuery, comp) - words.begin();
-
-            for (int j = idx; j < strQuery.size(); j++) {
-                strQuery[j] = 'z';
-            }
-            high = upper_bound(words.begin(), words.end(), strQuery, comp) - words.begin();
-
-        }
-        answer.push_back(high - low);
-
-    }
-    return answer;
-}
-
+// STL (Standard Template, Library)
+// 벡터, 스택, 페어
 int main() {
-    vector<string> words = { "frodo", "front", "frost", "frozen", "frame", "kakao" };
-    vector<string> queries = { "fro??", "????o", "fr???", "fro???", "pro?" };
-    //solution(words, queries);
+	string a;
+	int fir = 0, sec = 0;
+	int ipData = 0, mul = 10;
+	getline(cin, a);
+	stack<int> s;
+	
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] >= '0' && a[i] <= '9') {
+			ipData = (ipData * mul) + (a[i] - 48);
+		}
+		if (a[i] == ' ' && ipData != 0) {
+			s.push(ipData);
+			ipData = 0;
+		}
+
+		switch (a[i])
+		{
+		case '+':
+			fir = s.top();
+			s.pop();
+			sec = s.top();
+			s.pop();
+			s.push(sec + fir);
+			break;
+		case '-':
+			fir = s.top();
+			s.pop();
+			sec = s.top();
+			s.pop();
+			s.push(sec - fir);
+			break;
+		case '*':
+			fir = s.top();
+			s.pop();
+			sec = s.top();
+			s.pop();
+			s.push(sec * fir);
+			break;
+		case '/':
+			fir = s.top();
+			s.pop();
+			sec = s.top();
+			s.pop();
+			s.push(sec / fir);
+			break;
+		}
+	}
+
+	cout << s.top();
 }
