15bebe2c2fef829bcd538db3f5ff3d5c1fbab547
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-04-25 18:57:01 +0900

    map 패션왕

--- Algoritm/algorithm.cpp
+++ Algoritm/algorithm.cpp
@@ -37,29 +37,39 @@
 //	}
 //}
 #include <iostream>
-#include <set>
+#include <map>
 
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr); cout.tie(nullptr);
+	int N, res = 0;
+	map<string, int> m;
+	cin >> N;
 
-	set<int> s;
-	int n, m;
-	cin >> n >> m;
-	for (int i = 0; i < n + m; i++) {
-		int data;
-		cin >> data;
+	for (int i = 0; i < N; i++) {
+		int rep;
+		cin >> rep;
 
-		if (s.find(data) != s.end()) {
-			s.erase(s.find(data));
-			continue;
+		m.clear();
+		res = 1;
+
+		for (int j = 0; j < rep; j++) {
+			string clothes, type;
+			cin >> clothes >> type;
+		
+			if (m.find(type) == m.end()) { // 중복이 없는 경우
+				m.insert({type, 2});
+				continue;
+			}
+			
+			auto iter = m.find(type);
+			iter->second++;
 		}
-		s.insert(data);
-				
+
+		for (auto iter : m) {
+			res *= iter.second;
+		}
+
+		cout << res - 1 << "\n";
 	}
-
-	cout << s.size();
-
 }
