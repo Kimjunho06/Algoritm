280e4004eec4f82f79080f43faf7a16c64f666b8
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-05-01 19:54:16 +0900

    DP 피보나치

--- Algoritm/algorithm.cpp
+++ Algoritm/algorithm.cpp
@@ -1,69 +1,90 @@
-//#include <iostream>
-//#include <queue>
-//#include <functional>
-//
-//using namespace std;
-//
-//long long dq[5000001];
-//int main() {
-//	ios_base::sync_with_stdio(false);
-//	cin.tie(nullptr); cout.tie(nullptr);
-//
-//	int N, L;
-//	queue<long long> s;
-//	priority_queue<long long, vector<long long>, greater<>> pq;
-//	cin >> N >> L;
-//	
-//	for (int i = 0; i < N; i++) { // 최대 500만번
-//		long long data;
-//		cin >> data;
-//		
-//		dq[i] = data;
-//
-//		if (i < L) {
-//			pq.pop();
-//			pq.emplace(dq, dq + i + 1);
-//			s.push(pq.top()); // 21~22
-//		}
-//		else {
-//			priority_queue<long long, vector<long long> , greater<>> pq(dq + i - L + 1, dq + i+1);
-//			s.push(pq.top());
-//		}
-//	}
-//
-//	for (int i = 0; i < N; i++) {
-//		cout << s.front() << " ";
-//		s.pop();
-//	}
-//}
 #include <iostream>
-#include <map>
-#include <string>
 
 using namespace std;
 
-int main() {
-	map<string, float> mp;
-	int cnt = 0;
-	string a;
+int arr[46];
 
-	while (getline(cin, a))
-	{
-		// getline(cin, a);
-		cnt++;
-		if (mp.find(a) != mp.end()) { // 찾아지면
-			auto iter = mp.find(a);
-			iter->second++;
-			continue;
-		}
-		mp.insert({ a, 1 });
-		// if (a == "Willow") break;
+int F(int f, int arr[46]) {
+	if (arr[f] != 0) return arr[f];
+
+	int result = 0;
+
+	if (f <= 2)
+		result = 1;
+	else {
+		result = F(f - 2, arr) + F(f - 1, arr);
 	}
 
-	cout << fixed;
-	cout.precision(4);
+	arr[f] = result;
 
-	for (auto iter : mp) {
-		cout << iter.first << " " << (iter.second/cnt) * 100 << "\n";
-	}
+	return result;
 }
+
+int main() {
+	int n;
+	cin >> n;
+
+	cout << F(n, arr);
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ///////////////////////////////
+/*stack<char> s;
+	string str;
+
+	cin >> str;
+
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] >= 'A' && str[i] <= 'Z') {
+			cout << str[i];
+		}
+
+		if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
+			s.push(str[i]);
+		}
+
+
+	}
+
+	while (!s.empty())
+	{
+		cout << s.top();
+		s.pop();
+	}*/
+// G*(A-B*(C/D+E)/F)
+// GABCD/E+*F/-*
+
