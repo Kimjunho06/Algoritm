0b45a73192906016db34b7c9ce0732e880e68f06
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-05-11 16:02:40 +0900

    queue

--- Algoritm/Classalgorithm.cpp
+++ Algoritm/Classalgorithm.cpp
@@ -1,48 +1,170 @@
 #include <iostream>
-#include <cmath>
+#include <queue>
+
+using namespace std;
+
+//class Queue {
+//	int* buf;
+//	int qsize;
+//	int front;
+//	int rear;
+//
+//public:
+//	void InitQueue(int qsize) {
+//		this->qsize = qsize;
+//		buf = new int(qsize);
+//		front = -1;
+//		rear = -1;
+//	}
+//	void Enqueue(int data) {
+//		if (IsFull()) {
+//			cout << "꽉참" << endl;
+//		}
+//		else {
+//			++rear;
+//			buf[rear] = data;
+//		}
+//	}
+//	int Dequeue() {
+//		if (IsEmpty()) return -1;
+//		else {
+//			++front;
+//			return buf[front];
+//		}
+//	}
+//	int IsFull() {
+//		if (qsize - 1 == rear) {
+//			return 1;
+//		}
+//		return 0;
+//	}
+//	int IsEmpty() {
+//		if (rear == front) {
+//			return 1;
+//		}
+//		return 0;
+//	}
+//};
+//int main() {
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr); cout.tie(nullptr);
+//
+//	int i;
+//	Queue q1;
+//	q1.InitQueue(10);
+//
+//	for (int i = 1; i <= 11; i++) {
+//		cout << i << "입력" << endl;
+//		q1.Enqueue(i);
+//	}
+//	cout << endl;
+//	while (!q1.IsEmpty())
+//	{
+//		cout << q1.Dequeue() << "출력" << endl;
+//	}
+//	cout << endl;
+//	return 0;
+//}
+
+// vector 방식
+/*
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-	float arr[10][10];
-	float avg = 0, min = 100;
-	float result = 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
 
-	for (int i = 0; i < 9; i++) {
-		avg = 0;
-		min = 100;
-		for (int j = 0; j < 9; j++) {
-			cin >> arr[i][j];
-			avg += arr[i][j];
+	vector<int> v;
+	string s;
+	int n, data;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+
+		if (s == "push") {
+			cin >> data;
+			v.push_back(data);
+			continue;
 		}
-		avg /= 9;
-		avg = round(avg);
+		else if (s == "size") {
+			cout << v.size() << "\n";
+			continue;
+		}
+		else if (s == "empty") {
+			cout << v.empty() << "\n";
+			continue;
+		}
 
-		for (int j = 0; j < 9; j++) {
-			if (abs(avg - arr[i][j]) < min) {
-				min = abs(avg - arr[i][j]);
-				result = arr[i][j];
+		if (!v.empty()) {
+			if (s == "pop") {
+				cout << v.front() << "\n";
+				v.erase(v.begin() + 0);
+			}
+			else if (s == "front") {
+				cout << v.front() << "\n";
+			}
+			else if (s == "back") {
+				cout << v.back() << "\n";
 			}
 		}
-		cout << avg << " " << result << endl;
+		else {
+			cout << -1 << "\n";
+		}
 	}
+}
+*/
+// queue 방식
+/* queue 방식
+#include <iostream>
+#include <queue>
 
-	int N, sum = 0;
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	queue<int> q;
+	string a;
+	int N, data;
 	cin >> N;
-	for (int i = 1; i <= N; i++) {
-		sum = i;
-		for (int j = i + 1; j < N; j++) {
-			sum += j;
-			if (sum > N) break;
-			if (sum == N) {
-				while (i - 1 != j) {
-					cout << j;
-					j--;
-					if (i - 1 == j) break;
-					cout << " + ";
-				}
-				cout << " = " << N << endl;
+
+	while (N--)
+	{
+		cin >> a;
+		if (a == "push") {
+			cin >> data;
+			q.push(data);
+			continue;
+		}
+		else if (a == "size") {
+			cout << q.size() << "\n";
+			continue;
+		}
+		else if (a == "empty") {
+			cout << q.empty() << "\n";
+			continue;
+		}
+
+		if (q.empty()) {
+			cout << -1 << "\n";
+		}
+		else {
+			if (a == "pop") {
+				cout << q.front() << "\n";
+				q.pop();
+			}
+			else if (a == "front") {
+				cout << q.front() << "\n";
+			}
+			else if (a == "back") {
+				cout << q.back() << "\n";
 			}
 		}
 	}
 }
+*/
--- Algoritm/algorithm.cpp
+++ Algoritm/algorithm.cpp
@@ -1,22 +1,110 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <stack>
+
 using namespace std;
 
 int main()
 {
-    int n;
-    vector<int> v;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        int data;
-        cin >> data;
-        v.push_back(data);
-    }
+	string str;
+	string answer;
+	
+	stack<char> s;
+	cin >> str;
 
-    sort(v.begin(), v.end());
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] >= 'A' && str[i] <= 'Z') {
+			answer += str[i];
+		}
+		else if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
+			if (str[i] == '+' || str[i] == '-') {
+				if (!s.empty() && (s.top() == '*' || s.top() == '/')) {
+					while (!s.empty())
+					{
+						answer += s.top();
+						s.pop();
+					}
+					s.push(str[i]);
+				}
+				else {
+					s.push(str[i]);	
+				}
+			}
+			else {
+				s.push(str[i]);	
+			}
+		}
+	
+		if (str[i] == '(') {
+			s.push(str[i]);
+		}
+		if (str[i] == ')') {
+			int size = s.size();
+			for (int j = 0; j < size; j++) {
+				if (s.top() != '(') {
+					answer += s.top();
+				}
+				if (s.top() == '(') {
+					s.pop();
+					break;
+				}
+				s.pop();
+			}
+		}
+	}
 
-    for (auto a : v) {
-        cout << a << endl;
-    }
+	while (!s.empty())
+	{
+		answer += s.top();
+		s.pop();
+	}
+
+	cout << answer;
 }
+
+
+
+//int priority(char op) {
+//    switch (op) {
+//    case'(': case')': return 0;
+//    case '+': case'-': return 1;
+//    case '*':case'/': return 2;
+//    }
+//    return -1;
+//}
+//
+//string infixToPostfix(string a) {
+//    stack<char>s;
+//    string postfix = "";
+//    char ch, top_op;
+//    for (int i = 0; i < a.size(); i++) {
+//        ch = a[i];
+//        switch (ch) {
+//        case '+': case '-': case '*': case '/':
+//            while (// 채워넣으세요) {
+//                postfix += s.top();
+//                s.pop();
+//            }
+//            s.push(ch);
+//            break;
+//        case '(':
+//            s.push(ch);
+//            break;
+//        case ')':
+//            top_op = s.top();
+//            while (top_op != '(') { //왼쪽 괄호를 만날때까지 출력
+			   // 채워넣으세요
+//            }
+//            s.pop();
+//            break;
+//        default: //피연산자
+//            postfix += ch;
+//            break;
+//
+//        }
+//    }
+//    while (!s.empty()) {
+//        postfix += s.top();
+//        s.pop();
+//    }
+//    return postfix;
+//}
