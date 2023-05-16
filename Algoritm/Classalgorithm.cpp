14aa015b2148efe05e289d0fdef0461480c9d425
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-05-16 12:58:39 +0900

    연산

--- Algoritm/Classalgorithm.cpp
+++ Algoritm/Classalgorithm.cpp
@@ -1,33 +1,12 @@
-#include <vector>
-#include <queue>
 #include <iostream>
 
 using namespace std;
 
-vector<int> solution(vector<int> arr)
-{
-    vector<int> answer;
-    queue<int> q;
-
-    for (int i = 0; i < arr.size(); i++) {
-
-        if (q.empty()) {
-            q.push(arr[i]);
-        }
-        else {
-            if (q.front() != arr[i]) {
-                answer.push_back(q.front());
-                while (!q.empty()) {
-                    q.pop();
-                }
-                q.push(arr[i]);
-            }
-            else {
-                q.push(arr[i]);
-            }
-        }
-    }
-    answer.push_back(q.front());
-
-    return answer;
+int main() {
+	int n;
+	cin >> n;
+	
+	if (n == 1) cout << 0;
+	else if (n % 2 == 0) cout << (n * n) / 2;
+	else if (n % 2 == 1) cout << (n * n) / 2 + 1;
 }
