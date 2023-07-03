c1260dd1942f66564c66628162065dd5fbf59b94
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-07-03 13:14:18 +0900

    이항계수

--- Algoritm/Classalgorithm.cpp
+++ Algoritm/Classalgorithm.cpp
@@ -2,23 +2,18 @@
 
 using namespace std;
 
+int f(int num) {
+	if (num == 0) return 1;
+
+	return num * f(num - 1);
+}
+
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr); cout.tie(nullptr);
+	cin.tie(nullptr);
 
-	int arr[10002] = {};
-	int n, input;
-	cin >> n;
-	
-	for (int i = 0; i < n; i++) {
-		cin >> input;
-		arr[input]++;
-	}
-
-	for (int i = 0; i <= 10001; i++) {
-		while (arr[i]--)
-		{
-			cout << i << "\n";
-		}
-	}
+	int n, k;
+	cin >> n >> k;
+	cout << f(n) / f(k) / f(n - k);
+	return 0;
 }
