c0642af9781afa5939bb6084af89511ec9f6268a
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-07-03 11:06:35 +0900

    수학

--- Algoritm/Classalgorithm.cpp
+++ Algoritm/Classalgorithm.cpp
@@ -2,35 +2,18 @@
 
 using namespace std;
 
-int F(long long f, long long r, long long* arr) {
-	int floor = f - 1 < 0 ? 0 : f - 1;
+int main() {
+	int A, B, V;
+	int f, b;
+	cin >> A >> B >> V;
 
-	if (arr[14 * f + r] != 0) {
-		return arr[14 * f + r];
+	f = A - B;
+	b = V - B;
+
+	if (b % f == 0) {
+		cout << b / f;
 	}
 	else {
-		for (int i = 1; i <= r; i++) {
-			if (arr[14 * floor + i] == 0) {
-				F(floor, i, arr);
-			}
-			arr[14 * f + r] += arr[14 * floor + i];
-		}
-		return arr[14 * f + r];
-	}
-}
-
-int main() {
-	long long T, floor = 0, room = 0;
-	long long arr[500] = {};
-	cin >> T;
-
-	for (int i = 1; i <= 14; i++) {
-		arr[i] = i;
-	}
-
-	for (int i = 0; i < T; i++) 
-	{
-		cin >> floor >> room;
-		cout << F(floor, room, arr) << endl;
+		cout << b / f + 1;
 	}
 }
