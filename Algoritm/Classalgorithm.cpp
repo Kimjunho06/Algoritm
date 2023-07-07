057d6562ec8b6ceaa0cf2141beb41217fa6fad46
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-07-07 11:58:38 +0900

    수학

--- Algoritm/Classalgorithm.cpp
+++ Algoritm/Classalgorithm.cpp
@@ -8,18 +8,7 @@ int main() {
 
     int n, cnt = 0;
     cin >> n;
-    for (int i = 666;; i++) {
-        int tmp = i;
-        while (tmp >= 666) {
-            if (tmp % 1000 == 666) {
-                cnt++;
-                break;
-            }
-            tmp /= 10;
-        }
-        if (n == cnt) {
-            std::cout << i;
-            break;
-        }
-    }
+
+    cout << n / 5 + n / 25 + n / 125;
+
 }
