ada65c1d123ee95d011f10401d53fa3a69f21b49
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-07-08 23:35:11 +0900

    a+b

--- Algoritm/Classalgorithm.cpp
+++ Algoritm/Classalgorithm.cpp
@@ -6,9 +6,26 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    int n, cnt = 0;
-    cin >> n;
+    string str1, str2;
+    int arr[100002];
+    int f, s, input = 0, front = 0;
 
-    cout << n / 5 + n / 25 + n / 125;
+    cin >> str1 >> str2;
 
+    if (str1.size() < str2.size()) {
+        string tmp = str1;
+        str1 = str2;
+        str2 = tmp;
+    }
+
+    for (int i = 1; i <= str1.size()+1; i++) {
+        f = str1[i] - '0';
+        s = str1[i] = '0';
+
+        input = f + s;
+        if (input > 10) {
+
+        }
+        arr[i] = input % 10;
+    }
 }
