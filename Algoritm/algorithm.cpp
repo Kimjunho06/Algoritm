2cd0dbd11e1087050c489689258e2d6f54f100cb
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-07-09 23:53:45 +0900

    정렬 함수사용

--- Algoritm/algorithm.cpp
+++ Algoritm/algorithm.cpp
@@ -1,110 +1,23 @@
 #include <iostream>
-#include <stack>
+#include <algorithm>
 
 using namespace std;
 
-int main()
-{
-	string str;
-	string answer;
-	
-	stack<char> s;
-	cin >> str;
+int arr[1000001];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
 
-	for (int i = 0; i < str.size(); i++) {
-		if (str[i] >= 'A' && str[i] <= 'Z') {
-			answer += str[i];
-		}
-		else if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
-			if (str[i] == '+' || str[i] == '-') {
-				if (!s.empty() && (s.top() == '*' || s.top() == '/')) {
-					while (!s.empty())
-					{
-						answer += s.top();
-						s.pop();
-					}
-					s.push(str[i]);
-				}
-				else {
-					s.push(str[i]);	
-				}
-			}
-			else {
-				s.push(str[i]);	
-			}
-		}
-	
-		if (str[i] == '(') {
-			s.push(str[i]);
-		}
-		if (str[i] == ')') {
-			int size = s.size();
-			for (int j = 0; j < size; j++) {
-				if (s.top() != '(') {
-					answer += s.top();
-				}
-				if (s.top() == '(') {
-					s.pop();
-					break;
-				}
-				s.pop();
-			}
-		}
-	}
+    int n, m;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> m;
+        arr[i] = m;
+    }
 
-	while (!s.empty())
-	{
-		answer += s.top();
-		s.pop();
-	}
+    sort(arr, arr + n);
 
-	cout << answer;
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << "\n";
+    }
 }
-
-
-
-//int priority(char op) {
-//    switch (op) {
-//    case'(': case')': return 0;
-//    case '+': case'-': return 1;
-//    case '*':case'/': return 2;
-//    }
-//    return -1;
-//}
-//
-//string infixToPostfix(string a) {
-//    stack<char>s;
-//    string postfix = "";
-//    char ch, top_op;
-//    for (int i = 0; i < a.size(); i++) {
-//        ch = a[i];
-//        switch (ch) {
-//        case '+': case '-': case '*': case '/':
-//            while (// 채워넣으세요) {
-//                postfix += s.top();
-//                s.pop();
-//            }
-//            s.push(ch);
-//            break;
-//        case '(':
-//            s.push(ch);
-//            break;
-//        case ')':
-//            top_op = s.top();
-//            while (top_op != '(') { //왼쪽 괄호를 만날때까지 출력
-			   // 채워넣으세요
-//            }
-//            s.pop();
-//            break;
-//        default: //피연산자
-//            postfix += ch;
-//            break;
-//
-//        }
-//    }
-//    while (!s.empty()) {
-//        postfix += s.top();
-//        s.pop();
-//    }
-//    return postfix;
-//}
